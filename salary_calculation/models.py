753bc018d9b1329eec9c36d9594a0301b1a84264
unknown <rezamohamadlo@yahoo.com>
2025-04-17 23:52:45 +0330

    calculate tax

--- salary_calculation/models.py
+++ salary_calculation/models.py
@@ -9,23 +9,26 @@ class SalaryCalculation(models.Model):
     working_month = models.ForeignKey(WorkingMonth, blank=True, null=True, on_delete=models.PROTECT, verbose_name='ماه کاری')
     tax = models.IntegerField(default=0, editable=False, verbose_name='مالیات')
     insurance = models.IntegerField(default=0, editable=False, verbose_name='بیمه')
+    insurance_employee_share = models.IntegerField(default=0, editable=False, verbose_name='ییمه سهم کارگر')
+    insurance_employer_share = models.IntegerField(default=0, editable=False, verbose_name='ییمه سهم کارفرما')
     base_wage = models.IntegerField(default=0, editable=False, verbose_name='حقوق مبنا')
     overtime = models.IntegerField(default=0, editable=False, verbose_name='اضافه کار')
+
     # other fields
 
     def update_total(self):
+
+        #calculate tax
         total_salary = self.items.filter(salary_item__include_in_tax=True).aggregate(
             total=models.Sum('amount')
         )['total'] or 0
 
-        # Get or create SalaryTaxCalculation for this employee and working month
         tax_calculation, created = SalaryTaxCalculation.objects.get_or_create(
             salary_tax_calculation_employee=self.employee,
             salary_tax_calculation_working_month=self.working_month,
             defaults={'salary_tax_calculation_salary': total_salary}
         )
         
-        # If it already existed, update the salary and save to trigger tax calculation
         if not created:
             tax_calculation.salary_tax_calculation_salary = total_salary
             tax_calculation.save()
@@ -36,7 +39,8 @@ class SalaryCalculation(models.Model):
         self.insurance = self.items.filter(salary_item__include_in_insurance=True).aggregate(
             total=models.Sum('amount')
         )['total'] or 0
-
+        self.insurance_employee_share = self.insurance * 0.07
+        self.insurance_employer_share = self.insurance * 0.23   
         #calculate base_wage
         self.base_wage = self.items.filter(salary_item__include_in_base_wage=True).aggregate(
             total=models.Sum('amount')
